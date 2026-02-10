84ffb7368276ff9cf21aee757aa5401e27723af1
Iyaan <iyaanb60@gmail.com>
2026-02-10 12:39:13 +0000

    Output
    Welcoming user and telling user to press A to start the program

--- Traffic_lights/src/main/java/Main_program.java
+++ Traffic_lights/src/main/java/Main_program.java
@@ -5,6 +5,7 @@ public class Main_program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SwiftBotAPI robot = SwiftBotAPI.INSTANCE;
+		System.out.println("Welcome \n Please press A to start");
 	}
 
 }
