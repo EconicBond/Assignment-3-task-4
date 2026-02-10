3bc3872e589c567e56990401cff30693230eb173
Iyaan <iyaanb60@gmail.com>
2026-02-10 13:13:19 +0000

    Readding colour codes

--- Traffic_lights/src/main/java/Main_program.java
+++ Traffic_lights/src/main/java/Main_program.java
@@ -10,6 +10,11 @@ public class Main_program {
 		final boolean[] x = {false};
 		final boolean[] y = {false};
 		
+		int[] red = {255 , 0, 0};
+		int[] green = { 0, 255, 0};
+		int[] blue = { 0, 0, 255 };
+		int[] yellow = { 255, 255, 0 };
+		
 		double APressedStart = 0.0;
 		SwiftBotAPI robot = SwiftBotAPI.INSTANCE;
 		System.out.println("Welcome \n Please press A to start");
