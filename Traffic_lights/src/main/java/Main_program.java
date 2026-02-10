74e230943fedae8ff0b488ba80e27eaeed7399a1
Iyaan <iyaanb60@gmail.com>
2026-02-10 12:27:15 +0000

    Swiftbot instanced
    Instanced swiftbot API as only one instance is needed

--- Traffic_lights/src/main/java/Main_program.java
+++ Traffic_lights/src/main/java/Main_program.java
@@ -1,9 +1,10 @@
+import swiftbot.SwiftBotAPI;
 
 public class Main_program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		SwiftBotAPI robot = SwiftBotAPI.INSTANCE;
 	}
 
 }
